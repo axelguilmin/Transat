2e6d110e806e3a62ee48b9c4898577e8879676fa
Gaetan <gaetan@sybrix.fr>
2012-03-15 12:33:22 +0100

    udpate

--- applet/src/com/transat/client/applet/LoginPanel.java
+++ applet/src/com/transat/client/applet/LoginPanel.java
@@ -8,10 +8,18 @@
 
 package com.transat.client.applet;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -40,6 +48,8 @@ public class LoginPanel extends JPanel implements ActionListener
 	
 	private JLabel l_title = null;
 	
+	private Box b_center = null;
+	
 	private Box b_pseudo = null;
 	private JLabel l_pseudo = null;
 	private JTextField t_pseudo = null;
@@ -48,7 +58,7 @@ public class LoginPanel extends JPanel implements ActionListener
 	private JLabel l_password = null;
 	private JPasswordField t_password = null;
 
-	
+	private Button bt_ok = null;
 	
 	
 	
@@ -58,37 +68,51 @@ public class LoginPanel extends JPanel implements ActionListener
 	public LoginPanel()
 	{
 		super();
+				
+		this.setLayout(new BorderLayout());
 		
-		this.setBackground(Color.ORANGE);
-		
+		// TOP
 		// Title
 		this.l_title = new JLabel();
-		this.l_title.setHorizontalAlignment(JLabel.CENTER);
+		this.l_title.setHorizontalAlignment( JLabel.CENTER );
 		this.l_title.setText("еее SE CONNECTER еее");
 		this.l_title.setFont( new Font("Dialog", Font.PLAIN, 24) );
+		
+		// CENTER
 		// Pseudo
 		this.b_pseudo = new Box( BoxLayout.X_AXIS );
 		this.l_pseudo = new JLabel();
 		this.l_pseudo.setText("Pseudo");
 		this.l_pseudo.setFont( new Font("Dialog", Font.PLAIN, 18) );
 		this.t_pseudo = new JTextField();
-		this.t_pseudo.setColumns(25);
 		this.b_pseudo.add(this.l_pseudo);
 		this.b_pseudo.add(this.t_pseudo);
+		this.b_pseudo.setMaximumSize(new Dimension(300, 24));
 		// Password
 		this.b_password = new Box( BoxLayout.X_AXIS );
 		this.l_password = new JLabel();
 		this.l_password.setText("Password");
 		this.l_password.setFont( new Font("Dialog", Font.PLAIN, 18) );
 		this.t_password = new JPasswordField();
-		this.t_password.setColumns(25);
 		this.b_password.add(this.l_password);
 		this.b_password.add(this.t_password);
+		this.b_password.setMaximumSize(new Dimension(300, 24));
+		// Button OK
+		this.bt_ok = new Button("Me connecter");
+		this.bt_ok.setMaximumSize(new Dimension(300, 25));
+		this.bt_ok.addActionListener(this);
+		// Box center
+		this.b_center = new Box( BoxLayout.Y_AXIS );
+		this.b_center.setOpaque(true);
+		this.b_center.add(this.b_pseudo);
+		this.b_center.add(this.b_password);
+		this.b_center.add(this.bt_ok);
 		
 		
-		this.add(this.l_title);
-		//this.add(this.b_pseudo);
-		//this.add(this.b_password);
+		
+		this.add(this.l_title, BorderLayout.NORTH);
+		this.add(this.b_center, BorderLayout.CENTER);
+		
 	}
 
 
@@ -100,6 +124,25 @@ public class LoginPanel extends JPanel implements ActionListener
 	@Override
 	public void actionPerformed(ActionEvent event)
 	{
+		String dataUrl = "http://localhost:9000/user/read?pseudo=axel&password=axel";
+		//dataUrl = dataUrl.replace(" ", "%20");
+		URL 		url 		= null;
+		Object 		content 	= null;
+		String		response	= null;
+		try
+		{
+			url = new URL( dataUrl );
+			content = url.getContent();
+		}
+		catch (MalformedURLException e) { e.printStackTrace();}
+		catch (IOException e) { e.printStackTrace(); }
+		
+		if( content != null )
+		{
+			InputStream is	= (InputStream) content;
+			response = is.toString();
+			System.out.println(response);
+		}
 		
 	}
 	
---
+++ applet/src/com/transat/client/applet/SignupPanel.java
@@ -0,0 +1,21 @@
+package com.transat.client.applet;
+
+import javax.swing.JPanel;
+
+public class SignupPanel extends JPanel
+{
+	
+	
+	
+	//
+	//	CONSTRUCTOR
+	//
+	public SignupPanel()
+	{
+		super();
+		
+	}
+	
+	
+	
+}
